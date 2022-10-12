7daf3aee74d707a19152afddc3d35a531cc34115
Binnur KURT <deepcloudlabs@gmail.com>
2022-10-12 15:54:46 +0300

    initial commit

---
+++ module02-functional.programming.in.python/exercise03.py
@@ -0,0 +1,5 @@
+countries = [
+    {"code": "tur", "name": "turkey", "continent": "asia", "population": 80000000},
+    {"code": "fra", "name": "france", "continent": "europe", "population": 67000000},
+    {"code": "ita", "name": "italy", "continent": "europe", "population": 90000000}
+]
