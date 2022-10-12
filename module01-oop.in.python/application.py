239771eb3d84356df066ada2acc2e637995d5130
Binnur KURT <deepcloudlabs@gmail.com>
2022-10-12 11:46:24 +0300

    Update application.py

--- module01-oop.in.python/application.py
+++ module01-oop.in.python/application.py
@@ -1,12 +1,17 @@
-from banking.domain import Account, AccountStatus
+from banking.domain import Account, AccountStatus, InsufficientBalanceError
 
-acc1 = Account("TR1", 1000, AccountStatus.ACTIVE)
-acc2 = Account("TR2", 1000)
-acc3 = Account("TR3")
-print(acc1.iban)
-print(acc1.balance)
-print(acc1.status)
-# acc1.status = AccountStatus.CLOSED  # triggers the setter method
-# acc1.status = AccountStatus.BLOCKED  # triggers the setter method
-# acc1.withdraw(1000000)
-print(acc1.iban)
+try:
+    acc1 = Account("TR1", 1000, AccountStatus.ACTIVE)
+    acc2 = Account("TR2", 1000)
+    acc3 = Account("TR3")
+    print(acc1.iban)
+    print(acc1.balance)
+    print(acc1.status)
+    # acc1.status = AccountStatus.CLOSED  # triggers the setter method
+    # acc1.status = AccountStatus.BLOCKED  # triggers the setter method
+    acc1.withdraw(1000000)
+    print(acc1.iban)
+except ValueError as ve:
+    print(ve)
+except InsufficientBalanceError as ibe:
+    print(f"{ibe.message}: {ibe.deficit}")
