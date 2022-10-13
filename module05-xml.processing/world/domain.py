93871120e0b4c52f538dbae1608fe1bb41e20b46
Binnur KURT <deepcloudlabs@gmail.com>
2022-10-13 13:55:00 +0300

    initial commit

---
+++ module05-xml.processing/world/domain.py
@@ -0,0 +1,47 @@
+class Country:
+    def __init__(self):
+        self._code = ""
+        self._name = ""
+        self._continent = "Asia"
+        self._population = 0
+        self._surface_area = 0.0
+
+    @property
+    def code(self):
+        return self._code
+
+    @code.setter
+    def code(self, code):
+        self._code = code
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        self._name = name
+
+    @property
+    def continent(self):
+        return self._continent
+
+    @continent.setter
+    def continent(self, continent):
+        self._continent = continent
+
+    @property
+    def population(self):
+        return self._population
+
+    @population.setter
+    def population(self, population):
+        self._population = int(population)
+
+    @property
+    def surfaceArea(self):
+        return self._surface_area
+
+    @surfaceArea.setter
+    def surfaceArea(self, surfaceArea):
+        self._surface_area = float(surfaceArea)
