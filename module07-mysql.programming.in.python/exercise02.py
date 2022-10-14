9fefe5267f42eadfece822f51b05f969f8950855
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-14 10:54:17 +0300

    Update exercise02.py

--- module07-mysql.programming.in.python/exercise02.py
+++ module07-mysql.programming.in.python/exercise02.py
@@ -4,10 +4,36 @@ mysql_connection = connector.connect(
     host="localhost",
     user="root",
     password="Secret_123",
-    database="world"
+    database="banking"
 )
 
 my_cursor = mysql_connection.cursor()
-my_cursor.execute("show tables")
-for table_name in my_cursor:
-    print(table_name[0])
+my_cursor.execute("""
+    create table accounts(
+        iban varchar(48) primary key,
+        balance float default 10.0,
+        status enum('ACTIVE', 'CLOSED', 'BLOCKED') default 'ACTIVE'
+    ) engine=innodb
+""")
+
+"""
+mysql> use banking
+Database changed
+mysql> show tables;
++-------------------+
+| Tables_in_banking |
++-------------------+
+| accounts          |
++-------------------+
+1 row in set (0.00 sec)
+
+mysql> desc accounts;
++---------+-----------------------------------+------+-----+---------+-------+
+| Field   | Type                              | Null | Key | Default | Extra |
++---------+-----------------------------------+------+-----+---------+-------+
+| iban    | varchar(48)                       | NO   | PRI | NULL    |       |
+| balance | float                             | YES  |     | 10      |       |
+| status  | enum('ACTIVE','CLOSED','BLOCKED') | YES  |     | ACTIVE  |       |
++---------+-----------------------------------+------+-----+---------+-------+
+3 rows in set (0.00 sec)
+"""
