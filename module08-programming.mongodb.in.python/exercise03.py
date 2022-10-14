66cb6b97c650131e0d21dd9a39938f382e7e4d5a
deepcloudlabs <deepcloudlabs@gmail.com>
2022-10-14 13:58:17 +0300

    Update exercise03.py

--- module08-programming.mongodb.in.python/exercise03.py
+++ module08-programming.mongodb.in.python/exercise03.py
@@ -7,10 +7,11 @@ banking_db = mongo_client["banking"]
 accounts_collection = banking_db.accounts
 
 print("ALL ACTIVE ACCOUNTS:")
-for account in accounts_collection.find({"status": "ACTIVE"}):
+for account in accounts_collection.find({"status": "ACTIVE"}, {"balance": False}):
     print(account)
-    
+
 # http://binkurt.blogspot.com/2015/02/mongodb-ile-calsmak.html
 print("\nACCOUNTS (balance > 200k & status=CLOSED):")
-for account in accounts_collection.find({"$and": [{"balance": {"$gt": 200000}}, {"status": "BLOCKED"}]}):
+for account in accounts_collection.find({"$and": [{"balance": {"$gt": 200000}}, {"status": "BLOCKED"}]},
+                                        {"balance": True}):
     print(account)
